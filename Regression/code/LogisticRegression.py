4ad7a168e68edd261621236654065cde068716ea
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-07-01 12:11:09 -0700

    Update LogisticRegression.py

--- Regression/code/LogisticRegression.py
+++ Regression/code/LogisticRegression.py
@@ -8,13 +8,13 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 
-def inputs(m, n):
+def inputs(m):
 	'''
 	Define the placeholder for X and Y
 	:return: tensors for X and Y
 	'''
-	X = tf.compat.v1.placeholder(tf.float32, shape=[None, n])
-	Y = tf.compat.v1.placeholder(tf.float32, shape=[None, m])
+	X = tf.compat.v1.placeholder(tf.float32, shape=[None, m])
+	Y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
 
 	return X, Y
 
@@ -28,19 +28,15 @@ def hypothesis( X, W, b ):
 	'''
 	return tf.nn.sigmoid( tf.matmul(X, W) + b )
 
-def loss( X, Y, W, b ):
+def loss( y_hat, Y ):
 	'''
 	Create the loss/cost function
-	:param X: The input tensor (data).
+	:param y_hat: predicted Y
 	:param Y: The input tensor (label).
-	:param W: The weight variable.
-	:param b: The bias variable.
 	:return: the loss.
 	'''
-	y_hat = hypothesis(X, W, b) # also known as predicted Y
 
-	# Calculating the cost 
-	return tf.reduce_mean( Y*tf.log(y_hat) + (1-Y)*tf.log(1-y_hat) )
+	return - tf.reduce_mean( Y*tf.math.log(y_hat) + (1-Y)*tf.math.log(1-y_hat) )
 	# return tf.nn.sigmoid_cross_entropy_with_logits(logits = y_hat, labels = Y)
 
 def train( rate, loss ):
@@ -51,18 +47,19 @@ def train( rate, loss ):
 	:return: optimizing result
 	'''
 
-	opt = tf.train.GradientDescentOptimizer( learning_rate= rate )
+	opt = tf.compat.v1.train.GradientDescentOptimizer( learning_rate= rate )
 	return opt.minimize( loss )
 
-def accuracy( X, Y, W, b ):
+def accuracy( y_hat, Y ):
 	'''
 	Get the accuracy of the predicted value.
+	:param y_hat: predicted Y
+	:param Y: The input tensor (label).
 	:return: accuracy
 	'''
-	y_hat = hypothesis(X, W, b)
-	predicted = tf.equal( tf.argmax(y_hat, 1), tf.argmax(Y, 1) )
 
-	return tf.reduce_mean( tf.cast(predicted, dtype=tf.float32) )
+	predicted = tf.cast( y_hat > 0.5, dtype=tf.float32 )
+	return tf.reduce_mean( tf.cast( tf.equal(predicted, Y), dtype=tf.float32) )
 
 if __name__ == '__main__':
 	# Import the dataset
@@ -70,42 +67,43 @@ if __name__ == '__main__':
 	x = data.iloc[:, :-1].values 
 	y = data.iloc[:, -1:].values
 
-	n, m = len(x), len( set(y) )
+	n, m = len(x), len(x[0])
 
+	
 	## Initialize the training variables
 	# Two different ways to initialize: random or 0
-	W = tf.Variable(tf.random_normal([n, m]), name='weight')
-	b = tf.Variable(tf.random_normal([m]), name='bias')
-
-	# W = tf.Variable(0.0, name='weight')
-	# b = tf.Variable(0.0, name='bias')
+	W = tf.Variable(tf.random.normal([m, 1]), name='weight')
+	b = tf.Variable(tf.random.normal([1]), name='bias')
 
 	# Launch the graph in a session.
 	with tf.compat.v1.Session() as sess:
 		# Initialize the variables W and b.
-		sess.run( tf.global_variables_initializer() )
+		sess.run( tf.compat.v1.global_variables_initializer() )
 		alpha, epochs = 0.0035, 500
 
 		# Get the input tensors
-		X, Y = inputs( m, n )
+		X, Y = inputs( m )
 
-		cost = loss( X, Y, W, b )
-		optimizer = train( alpha, cost )
-		acc = accuracy( X, Y, W, b )
+		y_hat = hypothesis( X, W, b )
+		cost = loss( y_hat, Y )
+		opt = train( alpha, cost )
+		acc = accuracy( y_hat, Y )
 
 		for epoch in range( epochs ):
-			sess.run( optimizer, feed_dict = {X : x, Y : y} )
+			sess.run( opt, feed_dict = {X : x, Y : y} )
 			if epoch % 50 == 0:
 				c = sess.run( cost, feed_dict = {X : x, Y : y} ) 
 				a = sess.run( acc, feed_dict = {X : x, Y : y} ) 
-				print "Epoch : ", epoch, ", cost =", c, "accuracy =", a
+				print "Epoch : ", epoch, ", cost =", c, ", accuracy =", a
 		'''
 		# Get the optimized result
 		training_cost = sess.run( cost, feed_dict ={X: x, Y: y} ) 
 		weight = sess.run(W) 
 		bias = sess.run(b) 
-		print "Training cost =", training_cost, "Weight =", weight, "bias =", bias, '\n'
+		
+		print "Training cost =", training_cost, "Accuracy= ", traning_acc, "Weight =", weight, "bias =", bias, '\n'
 
+		
 		# Calculating the predictions 
 		predictions = weight * x + bias 
 
